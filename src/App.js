8b61393b86f23c1cc65eb061cae59826706c1a99
Doom <vnagorniyrnd@gmail.com>
2022-08-25 18:20:02 +0300

    Создание инпутов, создание своего компонента MyButton, MyInput, использование CSS-module, получение данных с помощью управляемого inputa

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 function App() {
@@ -11,9 +13,20 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]);
+  const [title, setTitle] = useState('sdsad');
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  };
 
   return (
     <div className='App'>
+      <form>
+        <MyInput value={title} onChange={(e) => setTitle(e.target.value)} type='text' placeholder='Название поста' />
+        <MyInput type='text' placeholder='Описание поста' />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title='Посты про JS' />
     </div>
   );
