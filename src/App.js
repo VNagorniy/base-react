1d32aee44fc08c28a46b809912670eff369ec91f
Doom <vnagorniyrnd@gmail.com>
2022-08-25 17:06:19 +0300

    Создание функционального и классового компонента

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { useState } from 'react';
+import ClassCounter from './components/ClassCounter';
+import Counter from './components/Counter';
 
 function App() {
-  const [likes, setLikes] = useState(5);
   const [value, setValue] = useState('ТЕКСТ');
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
 
   return (
     <div className='App'>
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input type='text' value={value} onChange={(event) => setValue(event.target.value)}></input>
-      <button onClick={increment}>Inc</button>
-      <button onClick={decrement}>Dec</button>
+      <ClassCounter />
     </div>
   );
 }
