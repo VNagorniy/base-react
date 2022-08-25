b25179ccc37a900fb3af124e235a90528106818b
Doom <vnagorniyrnd@gmail.com>
2022-08-25 17:42:04 +0300

    Создание компонентов PostItem/PostList, добавление стилей, работа со списками через map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import './styles/App.css';
 
 function App() {
-  const [value, setValue] = useState('ТЕКСТ');
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+  ]);
 
   return (
     <div className='App'>
-      <ClassCounter />
+      <PostList posts={posts} title='Посты про JS' />
     </div>
   );
 }
