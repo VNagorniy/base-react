7a66589cae874f86f4496ef48c867dc8bd053d85
Doom <vnagorniyrnd@gmail.com>
2022-08-25 19:09:05 +0300

    Создание нового поста и перенос его в отдельный компонент, реализация удаление постов через filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
+import PostForm from './components/PostForm';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
@@ -13,21 +14,19 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]);
-  const [title, setTitle] = useState('sdsad');
 
-  const addNewPost = (e) => {
-    e.preventDefault();
-    console.log(title);
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
   };
 
   return (
     <div className='App'>
-      <form>
-        <MyInput value={title} onChange={(e) => setTitle(e.target.value)} type='text' placeholder='Название поста' />
-        <MyInput type='text' placeholder='Описание поста' />
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts} title='Посты про JS' />
+      <PostForm create={createPost} />
+      <PostList remove={removePost} posts={posts} title='Посты про JS' />
     </div>
   );
 }
