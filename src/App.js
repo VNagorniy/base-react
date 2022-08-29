cbbd1b328b7e86befc77b2d95cb57ada932c1985
Doom <vnagorniyrnd@gmail.com>
2022-08-29 20:52:10 +0300

    Использование библиотеки react-router v5.2

--- src/App.js
+++ src/App.js
@@ -1,81 +1,28 @@
-import axios from 'axios';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import PostService from './API/PostService';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
-import PostFilter from './components/PostFilter';
-import PostForm from './components/PostForm';
-import PostItem from './components/PostItem';
-import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
-import Loader from './components/UI/Loader/Loader';
-import MyModal from './components/UI/MyModal/MyModal';
-import Pagination from './components/UI/pagination/Pagination';
-import MySelect from './components/UI/select/MySelect';
-import { useFetching } from './hooks/useFetching';
-import { usePosts } from './hooks/usePosts';
+import React from 'react';
 import './styles/App.css';
-import { getPageCount, getPagesArray } from './utils/pages';
+import { BrowserRouter, Link, Route, Switch, Redirect } from 'react-router-dom';
+import About from './pages/About';
+import Posts from './pages/Posts';
+import Navbar from './components/UI/Navbar/Navbar';
+import Error from './pages/Error';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({
-    sort: '',
-    query: '',
-  });
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
-    const totalCount = response.headers['x-total-count'];
-    setTotalPages(getPageCount(totalCount, limit));
-  });
-
-  useEffect(() => {
-    fetchPosts(limit, page);
-  }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
-  const changePage = (page) => {
-    setPage(page);
-    fetchPosts(limit, page);
-  };
-
   return (
-    <div className='App'>
-      <button onClick={fetchPosts}>GET POSTS</button>
-      <MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: '15px 0' }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h1>Произошла ошибка ${postError}</h1>}
-      {isPostsLoading ? (
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
-          <Loader />
-        </div>
-      ) : (
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS' />
-      )}
-      <Pagination page={page} changePage={changePage} totalPages={totalPages} />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/posts'>
+          <Posts />
+        </Route>
+        <Route path='/error'>
+          <Error />
+        </Route>
+        <Redirect to='/error' />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
